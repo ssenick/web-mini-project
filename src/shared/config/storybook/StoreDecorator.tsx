5218fef24f6446a81be579902b7fe3756d2bdf4c
ssenick <ssenick@ukr.net>
2023-10-10 19:53:25 -0600

    fixed code

--- src/shared/config/storybook/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator.tsx
@@ -2,10 +2,8 @@ import { type Decorator } from '@storybook/react'
 import { type StateSchema, StoreProvider } from 'app/povaiders/StorePorider'
 import { type DeepPartial } from '@reduxjs/toolkit'
 
-export const StoreDecorator = (initialState?: DeepPartial<StateSchema>): Decorator => {
-  return (Story) => (
+export const StoreDecorator = (initialState?: DeepPartial<StateSchema>): Decorator => (Story) => (
         <StoreProvider initialState={initialState }>
             {Story()}
         </StoreProvider>
-  )
-}
+)
--- src/shared/config/storybook/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator.tsx
@@ -5,13 +5,10 @@ const style: Record<string, string | boolean> = {
   padding: '15px'
 }
 
-export const ThemeDecorator = (theme: Theme) => {
-  const decorator: Decorator = (Story) => (
+export const ThemeDecorator = (theme: Theme): Decorator => (Story) => (
       <ThemeProvider initialTheme={theme}>
           <div className={`app ${theme}`} style={style}>
               <Story />
           </div>
       </ThemeProvider>
-  )
-  return decorator
-}
+)
