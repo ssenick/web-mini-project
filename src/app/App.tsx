1e971ee37027b8bcb5da4f2ccc3039b1df5630d5
ssenick <ssenick@ukr.net>
2023-10-26 19:41:43 -0600

    added react-responsive and functionality for adding a classes to App component depending on screen width

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
-import { Suspense, useEffect } from 'react'
+import { Suspense, useEffect, useMemo } from 'react'
 import './styles/index.scss'
-import { classNames } from 'shared/lib/classNames/classNames'
+import { useMediaQuery } from 'react-responsive'
+import { classNames, type Mods } from 'shared/lib/classNames/classNames'
 import { Header } from 'widgets/Header'
 import { AppRouter } from 'app/povaiders/Router'
 import { Sidebar } from 'widgets/Sidebar'
@@ -9,11 +10,28 @@ import { useDispatch } from 'react-redux'
 
 const App = (): JSX.Element => {
   const dispatch = useDispatch()
+  /// === ////////
+  const isPcScreen = useMediaQuery({ minWidth: 1200 })
+  const isTabletScreen = useMediaQuery({ maxWidth: 1200, minWidth: 768 })
+  const isMobilScreen = useMediaQuery({ maxWidth: 768, minWidth: 450 })
+  const isXSMobilScreen = useMediaQuery({ maxWidth: 450 })
+  /// === ////////
+
   useEffect(() => {
     dispatch(userActions.initAuthData())
   }, [dispatch])
+
+  /// === ////////
+  const mods: Mods = useMemo(() => ({
+    isPcScreen,
+    isTabletScreen,
+    isMobilScreen,
+    isXSMobilScreen
+  }), [isPcScreen, isTabletScreen, isMobilScreen, isXSMobilScreen])
+  /// === ////////
+
   return (
-        <div className={classNames('app', {}, [])}>
+        <div className={classNames('app', mods, [])}>
             <Suspense fallback=''>
                 <Header className='app__header'/>
                 <Sidebar className='app__sidebar'/>
