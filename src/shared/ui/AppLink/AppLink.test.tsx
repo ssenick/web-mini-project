cef89a50377b8bc32664767227e9c61cd4757b73
ssenick <ssenick@ukr.net>
2023-10-26 19:39:02 -0600

    added Avatar and AppLink test

---
+++ src/shared/ui/AppLink/AppLink.test.tsx
@@ -0,0 +1,11 @@
+import { screen } from '@testing-library/react'
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender'
+import { AppLink } from './AppLink'
+
+describe('AppLink.test', () => {
+  test('render', () => {
+    componentRender(<AppLink to={'/'}>{'text'}</AppLink>,
+      { })
+    expect(screen.getByText('text')).toBeInTheDocument()
+  })
+})
