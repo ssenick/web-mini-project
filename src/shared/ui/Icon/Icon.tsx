3aad514d916a0f02bcafdb8852c6e568d898da0c
ssenick <ssenick@ukr.net>
2023-11-10 20:49:02 -0700

    created Icon component

---
+++ src/shared/ui/Icon/Icon.tsx
@@ -0,0 +1,15 @@
+import type React from 'react'
+import { memo } from 'react'
+import { classNames } from 'shared/lib/classNames/classNames'
+import cls from './Icon.module.scss'
+
+interface IconProps {
+  className?: string
+  Svg: React.VFC<React.SVGProps<SVGSVGElement>>
+}
+
+export const Icon = memo(({ className, Svg }: IconProps) => {
+  return (
+        <Svg className={classNames(cls.Icon, {}, [className])}/>
+  )
+})
