18dae00aaf58e7f60da45a5cc38edae4bf4e21db
ssenick <ssenick@ukr.net>
2023-11-13 19:54:03 -0700

    added Skeleton component

---
+++ src/shared/ui/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,76 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Skeleton } from './Skeleton'
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator'
+import { Theme } from 'app/povaiders/ThemeProvaider'
+
+const meta: Meta<typeof Skeleton> = {
+  title: 'shared/Skeleton',
+  component: Skeleton,
+  parameters: {
+    layout: 'fullscreen'
+  },
+  // tags: ['autodocs'],
+  argTypes: {}
+
+}
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Normal: Story = {
+  args: {
+    width: 300,
+    height: 100
+
+  }
+}
+Normal.decorators = [ThemeDecorator(Theme.LIGHT)]
+
+export const Circle: Story = {
+  args: {
+    border: '50%',
+    width: 100,
+    height: 100
+
+  }
+}
+Circle.decorators = [ThemeDecorator(Theme.LIGHT)]
+
+export const NormalFunny: Story = {
+  args: {
+    width: 300,
+    height: 100
+
+  }
+}
+NormalFunny.decorators = [ThemeDecorator(Theme.FUNNY)]
+
+export const CircleFunny: Story = {
+  args: {
+    border: '50%',
+    width: 100,
+    height: 100
+
+  }
+}
+CircleFunny.decorators = [ThemeDecorator(Theme.FUNNY)]
+
+export const NormalDark: Story = {
+  args: {
+    width: 300,
+    height: 100
+
+  }
+}
+NormalDark.decorators = [ThemeDecorator(Theme.DARK)]
+
+export const CircleDark: Story = {
+  args: {
+    border: '50%',
+    width: 100,
+    height: 100
+
+  }
+}
+CircleDark.decorators = [ThemeDecorator(Theme.DARK)]
---
+++ src/shared/ui/Skeleton/Skeleton.tsx
@@ -0,0 +1,32 @@
+import { type CSSProperties, memo } from 'react'
+import { classNames } from 'shared/lib/classNames/classNames'
+import cls from './Skeleton.module.scss'
+
+interface SkeletonProps {
+  className?: string
+  width?: string | number
+  height?: string | number
+  border?: string
+}
+
+export const Skeleton = memo((props: SkeletonProps) => {
+  const {
+    className,
+    width,
+    height,
+    border
+  } = props
+
+  const styles: CSSProperties = {
+    width,
+    height,
+    borderRadius: border
+  }
+  return (
+        <div
+            className={classNames(cls.Skeleton, {}, [className])}
+        style={styles}>
+
+        </div>
+  )
+})
