f2dc616c7dcf12f2962a3369d3e05344a8290cb2
ssenick <ssenick@ukr.net>
2023-11-13 18:05:34 -0700

    added massage after click button

--- src/shared/ui/Code/Code.tsx
+++ src/shared/ui/Code/Code.tsx
@@ -1,19 +1,29 @@
-import { memo, useCallback } from 'react'
+import { memo, useCallback, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import CopyIcon from 'shared/assets/icons/copy.svg'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { Icon } from 'shared/ui/Icon/Icon'
+import { Text, TextFontSize } from 'shared/ui/Text/Text'
 import { Button, ButtonVariant } from '../Button/Button'
-import CopyIcon from 'shared/assets/icons/copy.svg'
 import cls from './Code.module.scss'
 
 interface CodeProps {
   className?: string
   text: string
 }
+const delay = 500
 
 export const Code = memo(({ className, text }: CodeProps) => {
+  const [isCopy, setIsCopy] = useState(false)
+  const { t } = useTranslation()
   const onCopy = useCallback(() => {
     void navigator.clipboard.writeText(text)
+    setIsCopy(true)
+    setTimeout(() => {
+      setIsCopy(false)
+    }, delay)
   }, [text])
+  console.log(isCopy)
   return (
 
         <div className={classNames(cls.Code, {}, [className])}>
@@ -21,6 +31,12 @@ export const Code = memo(({ className, text }: CodeProps) => {
                 <Button onClick={onCopy} className={cls.button_copy} variant={ButtonVariant.THEME_ICON}>
                     <Icon Svg={CopyIcon} className={cls.icon}/>
                 </Button>
+                {isCopy && (
+                    <div className={cls.massage}>
+                        <Text size={TextFontSize.L} title={t('Текст скопирован')} />
+                    </div>
+                )}
+
                 <code>
                     {text}
                 </code>
