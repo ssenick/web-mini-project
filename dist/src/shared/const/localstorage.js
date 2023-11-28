62b212a8a878ab068801ab6759b199735c6d80f0
ssenick <ssenick@ukr.net>
2023-11-27 21:06:25 -0700

    added const language in localstorage

--- dist/src/shared/const/localstorage.js
+++ dist/src/shared/const/localstorage.js
@@ -1,2 +1,4 @@
 export var USER_LOCALSTORAGE_KEY = 'user';
 export var THEME_LOCALSTORAGE_KEY = 'theme';
+export var VIEW_LOCALSTORAGE_KEY = 'theme';
+export var LANGUAGE_LOCALSTORAGE_KEY = 'language';
--- src/shared/const/localstorage.ts
+++ src/shared/const/localstorage.ts
@@ -1,2 +1,4 @@
 export const USER_LOCALSTORAGE_KEY = 'user'
 export const THEME_LOCALSTORAGE_KEY = 'theme'
+export const VIEW_LOCALSTORAGE_KEY = 'theme'
+export const LANGUAGE_LOCALSTORAGE_KEY = 'language'
