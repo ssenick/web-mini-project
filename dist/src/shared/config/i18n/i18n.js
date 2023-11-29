14ee6669ed0896abfd9c8a16bfe9970eb467daeb
ssenick <ssenick@ukr.net>
2023-11-28 21:37:22 -0700

    fixed i18 in localstorage

--- dist/src/shared/config/i18n/i18n.js
+++ dist/src/shared/config/i18n/i18n.js
@@ -1,20 +1,21 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
 import { LANGUAGE_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 // import LanguageDetector from 'i18next-browser-languagedetector'
+var initialLanguage = 'ru';
+if (!localStorage.getItem(LANGUAGE_LOCALSTORAGE_KEY)) {
+    localStorage.setItem(LANGUAGE_LOCALSTORAGE_KEY, initialLanguage);
+}
 void i18n
     .use(Backend)
     // .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-    fallbackLng: 'ru',
+    fallbackLng: localStorage.getItem(LANGUAGE_LOCALSTORAGE_KEY) || initialLanguage,
     debug: __IS_DEV__,
     interpolation: {
         escapeValue: false
     }
 });
-if (!localStorage.getItem(LANGUAGE_LOCALSTORAGE_KEY)) {
-    localStorage.setItem(LANGUAGE_LOCALSTORAGE_KEY, 'ru');
-}
 export default i18n;
--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -4,15 +4,17 @@ import Backend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
 import { LANGUAGE_LOCALSTORAGE_KEY } from 'shared/const/localstorage'
 // import LanguageDetector from 'i18next-browser-languagedetector'
+const initialLanguage = 'ru'
+
 if (!localStorage.getItem(LANGUAGE_LOCALSTORAGE_KEY)) {
-  localStorage.setItem(LANGUAGE_LOCALSTORAGE_KEY, 'ru')
+  localStorage.setItem(LANGUAGE_LOCALSTORAGE_KEY, initialLanguage)
 }
 void i18n
   .use(Backend)
   // .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: localStorage.getItem(LANGUAGE_LOCALSTORAGE_KEY) || 'ru',
+    fallbackLng: localStorage.getItem(LANGUAGE_LOCALSTORAGE_KEY) || initialLanguage,
     debug: __IS_DEV__,
 
     interpolation: {
