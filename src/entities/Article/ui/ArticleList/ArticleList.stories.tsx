223d1e67a285c78693851cd0ff88742fb677926e
ssenick <ssenick@ukr.net>
2023-11-29 22:15:42 -0700

    added app__container

--- src/entities/Article/ui/ArticleList/ArticleList.stories.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.stories.tsx
@@ -78,6 +78,80 @@ const articles = [
         ]
       }
     ]
+  },
+  {
+    id: '2',
+    title: 'Java or Javascript',
+    subtitle: 'JavaScript new Keyword',
+    img: Image,
+    views: 1022,
+    createdAt: '26.02.2022',
+    user: {
+      username: 'ssenick',
+      avatar: Image,
+      id: '1'
+    },
+    type: [ArticleType.IT, ArticleType.SCIENCE],
+    blocks: [
+      {
+        id: '1',
+        type: ArticleBlockType.TEXT,
+        title: 'Lorem ipsum dolor.',
+        paragraphs: [
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, modi.',
+          'Lastly, the newly created object is returned. Note that func() does not include areturn statement. The compiler will implicitly insert ‘return this’ at the end.',
+          "Whether you're preparing for your first job interview or aiming to upskill in this ever-evolving tech landscape, GeeksforGeeks Courses are your key to success. "
+        ]
+      },
+      {
+        id: '4',
+        type: ArticleBlockType.CODE,
+        code: '<!DOCTYPE html>\n<html>\n  <body>\n    <p id="hello"></p>\n\n    <script>\n      document.getElementById("hello").innerHTML = "Hello, world!";\n    </script>\n  </body>\n</html>;'
+      },
+      {
+        id: '5',
+        type: ArticleBlockType.TEXT,
+        title: 'Заголовок этого блока',
+        paragraphs: [
+          'So new empty object will now include ‘a’ property. The func() also includes ‘c’ variable which is not declared with this keyword. So ‘c’ will not be included in new object. Lastly, the newly created object is returned. Note that func() does not include areturn statement. The compiler will implicitly insert ‘return this’ at the end.',
+          'So, the new object will also include ‘b’ property. Then it binds all the properties and functions declared with this keyword to a new empty object. Here, func() includes only one property ‘a’ which is declared with this keyword.'
+        ]
+      },
+      {
+        id: '2',
+        type: ArticleBlockType.IMAGE,
+        src: Image,
+        title: 'Image 1'
+      },
+      {
+        id: '3',
+        type: ArticleBlockType.CODE,
+        code: "const path = require('path');\n\nconst server = jsonServer.create();\n\nconst router = jsonServer.router(path.resolve(__dirname, 'db.json'));\n\nserver.use(jsonServer.defaults({}));\nserver.use(jsonServer.bodyParser);"
+      },
+      {
+        id: '7',
+        type: ArticleBlockType.TEXT,
+        title: 'Заголовок этого блока',
+        paragraphs: [
+          'Lastly, the newly created object is returned. Note that func() does not include areturn statement. The compiler will implicitly insert ‘return this’ at the end.',
+          "Whether you're preparing for your first job interview or aiming to upskill in this ever-evolving tech landscape, GeeksforGeeks Courses are your key to success. "
+        ]
+      },
+      {
+        id: '8',
+        type: ArticleBlockType.IMAGE,
+        src: Image,
+        title: 'image 2'
+      },
+      {
+        id: '9',
+        type: ArticleBlockType.TEXT,
+        title: 'Java',
+        paragraphs: [
+          'So new empty object will now include ‘a’ property. The func() also includes ‘c’ variable which is not declared with this keyword. So ‘c’ will not be included in new object. Lastly, the newly created object is returned. Note that func() does not include areturn statement. The compiler will implicitly insert ‘return this’ at the end.'
+        ]
+      }
+    ]
   }
 ] as Article[]
 const meta: Meta<typeof ArticleList> = {
@@ -123,28 +197,34 @@ Funny.decorators = [ThemeDecorator(Theme.FUNNY), StoreDecorator(), RouterDecorat
 
 export const FunnyBig: Story = {
   args: {
-    isLoading: false,
-    view: ArticleView.BIG,
-    articles
-  }
+  },
+  render: () => (
+      <div className='app__content'>
+        <ArticleList articles={articles} view={ArticleView.BIG}/>
+      </div>
+  )
 }
 FunnyBig.decorators = [ThemeDecorator(Theme.FUNNY), StoreDecorator(), RouterDecorator]
 
 export const LightBig: Story = {
   args: {
-    isLoading: false,
-    view: ArticleView.BIG,
-    articles
-  }
+  },
+  render: () => (
+      <div className='app__content'>
+        <ArticleList articles={articles} view={ArticleView.BIG}/>
+      </div>
+  )
 }
 LightBig.decorators = [ThemeDecorator(Theme.LIGHT), StoreDecorator(), RouterDecorator]
 
 export const DarkBig: Story = {
   args: {
-    isLoading: false,
-    view: ArticleView.BIG,
-    articles
-  }
+  },
+  render: () => (
+      <div className='app__content'>
+        <ArticleList articles={articles} view={ArticleView.BIG}/>
+      </div>
+  )
 }
 DarkBig.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator(), RouterDecorator]
 
@@ -159,15 +239,11 @@ IsLoadingSmall.decorators = [ThemeDecorator(Theme.LIGHT), StoreDecorator(), Rout
 
 export const IsLoadingBig: Story = {
   args: {
-    isLoading: true,
-    view: ArticleView.BIG,
-    articles
   },
   render: () => (
-      <div className={'app__content'}>
-        <ArticleList articles={articles} isLoading={true}/>
+      <div className='app__content'>
+        <ArticleList articles={[]} view={ArticleView.BIG} isLoading={true}/>
       </div>
-
   )
 }
 IsLoadingBig.decorators = [ThemeDecorator(Theme.LIGHT), StoreDecorator(), RouterDecorator]
--- src/features/ArticlesPageWrapper/ui/ArticlesPageWrapper/ArticlesPageWrapper.stories.tsx
+++ src/features/ArticlesPageWrapper/ui/ArticlesPageWrapper/ArticlesPageWrapper.stories.tsx
@@ -79,6 +79,80 @@ const articles = {
         ]
       }
     ]
+  },
+  2: {
+    id: '2',
+    title: 'Java or Javascript',
+    subtitle: 'JavaScript new Keyword',
+    img: Image,
+    views: 1022,
+    createdAt: '26.02.2022',
+    user: {
+      username: 'ssenick',
+      avatar: Image,
+      id: '1'
+    },
+    type: [ArticleType.IT, ArticleType.SCIENCE],
+    blocks: [
+      {
+        id: '1',
+        type: ArticleBlockType.TEXT,
+        title: 'Lorem ipsum dolor.',
+        paragraphs: [
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, modi.',
+          'Lastly, the newly created object is returned. Note that func() does not include areturn statement. The compiler will implicitly insert ‘return this’ at the end.',
+          "Whether you're preparing for your first job interview or aiming to upskill in this ever-evolving tech landscape, GeeksforGeeks Courses are your key to success. "
+        ]
+      },
+      {
+        id: '4',
+        type: ArticleBlockType.CODE,
+        code: '<!DOCTYPE html>\n<html>\n  <body>\n    <p id="hello"></p>\n\n    <script>\n      document.getElementById("hello").innerHTML = "Hello, world!";\n    </script>\n  </body>\n</html>;'
+      },
+      {
+        id: '5',
+        type: ArticleBlockType.TEXT,
+        title: 'Заголовок этого блока',
+        paragraphs: [
+          'So new empty object will now include ‘a’ property. The func() also includes ‘c’ variable which is not declared with this keyword. So ‘c’ will not be included in new object. Lastly, the newly created object is returned. Note that func() does not include areturn statement. The compiler will implicitly insert ‘return this’ at the end.',
+          'So, the new object will also include ‘b’ property. Then it binds all the properties and functions declared with this keyword to a new empty object. Here, func() includes only one property ‘a’ which is declared with this keyword.'
+        ]
+      },
+      {
+        id: '2',
+        type: ArticleBlockType.IMAGE,
+        src: Image,
+        title: 'Image 1'
+      },
+      {
+        id: '3',
+        type: ArticleBlockType.CODE,
+        code: "const path = require('path');\n\nconst server = jsonServer.create();\n\nconst router = jsonServer.router(path.resolve(__dirname, 'db.json'));\n\nserver.use(jsonServer.defaults({}));\nserver.use(jsonServer.bodyParser);"
+      },
+      {
+        id: '7',
+        type: ArticleBlockType.TEXT,
+        title: 'Заголовок этого блока',
+        paragraphs: [
+          'Lastly, the newly created object is returned. Note that func() does not include areturn statement. The compiler will implicitly insert ‘return this’ at the end.',
+          "Whether you're preparing for your first job interview or aiming to upskill in this ever-evolving tech landscape, GeeksforGeeks Courses are your key to success. "
+        ]
+      },
+      {
+        id: '8',
+        type: ArticleBlockType.IMAGE,
+        src: Image,
+        title: 'image 2'
+      },
+      {
+        id: '9',
+        type: ArticleBlockType.TEXT,
+        title: 'Java',
+        paragraphs: [
+          'So new empty object will now include ‘a’ property. The func() also includes ‘c’ variable which is not declared with this keyword. So ‘c’ will not be included in new object. Lastly, the newly created object is returned. Note that func() does not include areturn statement. The compiler will implicitly insert ‘return this’ at the end.'
+        ]
+      }
+    ]
   }
 }
 
@@ -105,7 +179,7 @@ LightSmall.decorators = [ThemeDecorator(Theme.LIGHT), StoreDecorator({
     isLoading: false,
     error: '',
     view: ArticleView.SMALL,
-    ids: ['1']
+    ids: ['1', '2']
   }
 }), RouterDecorator]
 
@@ -118,7 +192,7 @@ DarkSmall.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
     isLoading: false,
     error: '',
     view: ArticleView.SMALL,
-    ids: ['1']
+    ids: ['1', '2']
   }
 }), RouterDecorator]
 
@@ -131,12 +205,17 @@ FunnySmall.decorators = [ThemeDecorator(Theme.FUNNY), StoreDecorator({
     isLoading: false,
     error: '',
     view: ArticleView.SMALL,
-    ids: ['1']
+    ids: ['1', '2']
   }
 }), RouterDecorator]
 
 export const LightBig: Story = {
-  args: {}
+  args: {},
+  render: () => (
+      <div className='app__content'>
+        <ArticlesPageWrapper/>
+      </div>
+  )
 }
 LightBig.decorators = [ThemeDecorator(Theme.LIGHT), StoreDecorator({
   articlePage: {
@@ -144,12 +223,17 @@ LightBig.decorators = [ThemeDecorator(Theme.LIGHT), StoreDecorator({
     isLoading: false,
     error: '',
     view: ArticleView.BIG,
-    ids: ['1']
+    ids: ['1', '2']
   }
 }), RouterDecorator]
 
 export const DarkBig: Story = {
-  args: {}
+  args: {},
+  render: () => (
+      <div className='app__content'>
+        <ArticlesPageWrapper/>
+      </div>
+  )
 }
 DarkBig.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
   articlePage: {
@@ -157,12 +241,17 @@ DarkBig.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
     isLoading: false,
     error: '',
     view: ArticleView.BIG,
-    ids: ['1']
+    ids: ['1', '2']
   }
 }), RouterDecorator]
 
 export const FunnyBig: Story = {
-  args: {}
+  args: {},
+  render: () => (
+      <div className='app__content'>
+        <ArticlesPageWrapper/>
+      </div>
+  )
 }
 FunnyBig.decorators = [ThemeDecorator(Theme.FUNNY), StoreDecorator({
   articlePage: {
@@ -170,7 +259,7 @@ FunnyBig.decorators = [ThemeDecorator(Theme.FUNNY), StoreDecorator({
     isLoading: false,
     error: '',
     view: ArticleView.BIG,
-    ids: ['1']
+    ids: ['1', '2']
   }
 }), RouterDecorator]
 
@@ -183,12 +272,17 @@ IsLoadingSmall.decorators = [ThemeDecorator(Theme.LIGHT), StoreDecorator({
     isLoading: true,
     error: '',
     view: ArticleView.SMALL,
-    ids: ['1']
+    ids: ['1', '2']
   }
 }), RouterDecorator]
 
 export const IsLoadingBig: Story = {
-  args: {}
+  args: {},
+  render: () => (
+      <div className='app__content'>
+        <ArticlesPageWrapper/>
+      </div>
+  )
 }
 IsLoadingBig.decorators = [ThemeDecorator(Theme.LIGHT), StoreDecorator({
   articlePage: {
@@ -196,7 +290,7 @@ IsLoadingBig.decorators = [ThemeDecorator(Theme.LIGHT), StoreDecorator({
     isLoading: true,
     error: '',
     view: ArticleView.BIG,
-    ids: ['1']
+    ids: ['1', '2']
   }
 }), RouterDecorator]
 
@@ -209,6 +303,6 @@ Error.decorators = [ThemeDecorator(Theme.LIGHT), StoreDecorator({
     isLoading: false,
     error: 'error',
     view: ArticleView.BIG,
-    ids: ['1']
+    ids: ['1', '2']
   }
 }), RouterDecorator]
