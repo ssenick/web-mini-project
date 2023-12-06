cdb14b854664ade12ca4dbd75a8187efe9cfc5f0
ssenick <ssenick@ukr.net>
2023-12-05 19:15:20 -0700

    added useDebounce.ts, useThrottle.ts

---
+++ src/shared/lib/hooks/useDebounce.ts
@@ -0,0 +1,22 @@
+import { useCallback, useEffect, useRef } from 'react'
+
+export function useDebounce (callback: (...args: any[]) => void, delay: number): () => void {
+  const timer = useRef<ReturnType<typeof setInterval> | null>(null)
+  const debounceCallback = useCallback(() => {
+    if (timer.current) {
+      clearTimeout(timer.current)
+    }
+    timer.current = setTimeout((...args) => {
+      callback(...args)
+    }, delay)
+  }, [callback, delay])
+
+  useEffect(() => {
+    return () => {
+      if (timer.current) {
+        clearTimeout(timer.current)
+      }
+    }
+  }, [])
+  return debounceCallback
+}
---
+++ src/shared/lib/hooks/useThrottle.ts
@@ -0,0 +1,24 @@
+import { useCallback, useEffect, useRef } from 'react'
+
+export function useThrottle (callback: (...args: any[]) => void, delay: number): () => void {
+  const throttleRef = useRef<null | boolean>(null)
+  const throttleTimeout = useRef<ReturnType<typeof setInterval> | null>(null)
+  const throttledCallback = useCallback((...args: any[]) => {
+    if (!throttleRef.current) {
+      callback(...args)
+      throttleRef.current = true
+      throttleTimeout.current = setTimeout(() => {
+        throttleRef.current = false
+      }, delay)
+    }
+  }, [delay, callback])
+
+  useEffect(() => {
+    return () => {
+      if (throttleTimeout.current) {
+        clearTimeout(throttleTimeout.current)
+      }
+    }
+  }, [])
+  return throttledCallback
+}
