d5d4648326872db3bf69712a2d5b20153f4a1c7f
ssenick <ssenick@ukr.net>
2023-12-05 19:24:13 -0700

    1.AboutPage: fixed stories AboutPage; 2.fetchCommentsByArticleId: added a check for an empty array; 3.LoginModal: removed form clearing when closing login form; 4.useDebounce, useThrottle: commented out the ts error

--- dist/src/features/AuthByUsername/ui/LoginModal/LoginModal.js
+++ dist/src/features/AuthByUsername/ui/LoginModal/LoginModal.js
@@ -10,21 +10,11 @@ var __assign = (this && this.__assign) || function () {
     return __assign.apply(this, arguments);
 };
 import { jsx as _jsx } from "react/jsx-runtime";
-import { memo, Suspense, useEffect } from 'react';
-import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch';
+import { memo, Suspense } from 'react';
 import { LoaderPoints } from 'shared/ui/LoaderPoints/LoaderPoints';
 import { Modal } from 'shared/ui/Modal/Modal';
-import { loginActions } from '../../model/slice/loginSlice';
 import { LoginFormAsync } from '../LoginForm/LoginFormAsync';
 export var LoginModal = memo(function (_a) {
     var className = _a.className, isOpen = _a.isOpen, onClose = _a.onClose, isCloseModal = _a.isCloseModal;
-    var dispatch = useAppDispatch();
-    useEffect(function () {
-        if (!isOpen) {
-            dispatch(loginActions.setUsername(''));
-            dispatch(loginActions.setPassword(''));
-            dispatch(loginActions.setError());
-        }
-    }, [dispatch, isOpen]);
     return (_jsx(Modal, __assign({ className: className, isOpen: isOpen, onClose: onClose, lazy: true, isCloseModal: isCloseModal }, { children: _jsx(Suspense, __assign({ fallback: _jsx(LoaderPoints, {}) }, { children: _jsx(LoginFormAsync, {}) })) })));
 });
--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,8 +1,7 @@
-import { memo, Suspense, useEffect } from 'react'
-import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch'
+import { memo, Suspense } from 'react'
 import { LoaderPoints } from 'shared/ui/LoaderPoints/LoaderPoints'
 import { Modal } from 'shared/ui/Modal/Modal'
-import { loginActions } from '../../model/slice/loginSlice'
+
 import { LoginFormAsync } from '../LoginForm/LoginFormAsync'
 
 interface LoginModalProps {
@@ -13,15 +12,6 @@ interface LoginModalProps {
 }
 
 export const LoginModal = memo(({ className, isOpen, onClose, isCloseModal }: LoginModalProps) => {
-  const dispatch = useAppDispatch()
-  useEffect(() => {
-    if (!isOpen) {
-      dispatch(loginActions.setUsername(''))
-      dispatch(loginActions.setPassword(''))
-      dispatch(loginActions.setError())
-    }
-  }, [dispatch, isOpen])
-
   return (
         <Modal
             className={className}
