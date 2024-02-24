b9cfb9df6ad1abb9ddf9a53e252d82f8db107e6a
ssenick <ssenick@ukr.net>
2024-02-23 18:25:40 -0700

    fixed type ReducersList

--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -2,10 +2,10 @@ import { type Reducer } from '@reduxjs/toolkit'
 import { type ReactNode, useEffect } from 'react'
 import { useDispatch, useStore } from 'react-redux'
 
-import { type ReduxStoreWithManager, type StateSchemaKey } from '@/app/povaiders/StoreProvaider'
+import { type ReduxStoreWithManager, type StateSchema, type StateSchemaKey } from '@/app/povaiders/StoreProvaider'
 
 export type ReducersList = {
-  [name in StateSchemaKey]?: Reducer;
+  [name in StateSchemaKey]?: Reducer<NonNullable<StateSchema[name]>>;
 }
 
 // type ReducersListEntry = [StateSchemaKey, Reducer]
