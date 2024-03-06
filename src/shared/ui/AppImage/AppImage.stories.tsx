79447144a9e32d879fdedd8f72f6efacabf7fa16
ssenick <ssenick@ukr.net>
2024-03-05 18:15:55 -0700

    created AppImage component

---
+++ src/shared/ui/AppImage/AppImage.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Theme } from '@/app/povaiders/ThemeProvaider'
+import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator'
+
+import { AppImage } from './AppImage'
+
+const meta: Meta<typeof AppImage> = {
+  title: 'shared/AppImage',
+  component: AppImage,
+  parameters: {
+    layout: 'fullscreen'
+  },
+  // tags: ['autodocs'],
+  argTypes: {}
+
+}
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Light: Story = {
+  args: {}
+}
+Light.decorators = [ThemeDecorator(Theme.LIGHT)]
