fcac74e733b4d0882dee270d6a6a952885bdcb14
ssenick <ssenick@ukr.net>
2024-03-19 19:36:01 -0600

    fixed style mobile fullscreen ios

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,19 +3,30 @@ import './styles/index.scss';
 import { Suspense, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { useMediaQueryValues } from '@/app/povaiders/MediaQueryProvaider';
 import { AppRouter } from '@/app/povaiders/Router';
 import { getUserInited, userActions } from '@/entities/User';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Header } from '@/widgets/Header';
 import { Sidebar } from '@/widgets/Sidebar';
 
+const fixHeight = (): void => {
+   const vh = window.innerHeight * 0.01;
+   document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+
 const App = (): JSX.Element => {
    const dispatch = useDispatch();
    const inited = useSelector(getUserInited);
+   const { isMobile } = useMediaQueryValues();
 
    useEffect(() => {
       dispatch(userActions.initAuthData());
-   }, [dispatch]);
+
+      if (isMobile) {
+         fixHeight();
+      }
+   }, [dispatch, isMobile]);
 
    return (
       <div className={classNames('app', {}, [])}>
